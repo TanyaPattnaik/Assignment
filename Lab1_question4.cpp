efdf960c32dbbc825a4bb43ec7b08d578e58d62a
TanyaPattnaik <31205790+TanyaPattnaik@users.noreply.github.com>
2017-08-21 17:21:37 +0530

    Create Lab1_question4.cpp

---
+++ Lab1_question4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+	double l,b,peri;
+	cout << "Enter length and breadth of rectangle" << "\n";
+	cin >> l >> b;
+	peri=2*(l+b);
+	cout << "The area is " << peri;
+	return 0;
+}
