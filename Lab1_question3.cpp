b2bf5e1a4754ed4e5756af02c743ad5d079b1c7f
TanyaPattnaik <31205790+TanyaPattnaik@users.noreply.github.com>
2017-08-21 17:10:50 +0530

    Create Lab1_question3.cpp

---
+++ Lab1_question3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+	double a,b,sum,diff,product,quotient;
+	cout << "Enter two numbers";
+	cin >> a >> b;
+	sum=a+b;
+	diff=a-b;
+	product=a*b;
+	quotient=a/b;
+	cout << "The sum =" << sum <<"\n";
+	cout << "The difference is =" << diff << "\n";
+	cout << "The product is =" << product << "\n";
+	cout << "The quotient is =" << quotient << "\n";
+	return 0;
+}
