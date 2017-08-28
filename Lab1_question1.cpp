998aa6dd2b6114033f3715bff958d89c4ee076b3
TanyaPattnaik <31205790+TanyaPattnaik@users.noreply.github.com>
2017-08-28 14:43:44 +0530

    Update Lab1_question1.cpp

--- Lab1_question1.cpp
+++ Lab1_question1.cpp
@@ -4,14 +4,18 @@ int main() {
 	int i;
 	double d;
 	char c;
+	bool b;
 	cout << "Enter an integer" <<"\n";
 	cin >> i;
 	cout << "Enter a decimal" <<"\n";
 	cin >> d;
 	cout << "Enter a character" <<"\n";
 	cin >> c;
+	cout << "Enter boolean variable" <<"\n";
+	cin >> b;
 	cout << i << " is an integer" <<"\n";
 	cout << d << " is a decimal"<<"\n";
 	cout << c << " is a character"<<"\n";
+	cout << b << " is a boolean" << "\n";
 	return 0;
 }
